cd55ab7118f9169cf7dff89a3a40ce7b9d7a23d6
Lokesh Patidar <lokeshpatidar194@gmail.com>
2025-11-26 11:38:21 +0530

    Update OddEven.c

--- OddEven.c
+++ OddEven.c
@@ -17,3 +17,24 @@ int main(){
     
     return 0;
 }
+
+
+
+// Q Number is divisible by 5.
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("Enter a number : ");
+    scanf("%d", &n);
+    
+    if(n%5 == 0){
+        printf("Divisible by 5");
+    }
+    else{
+        printf("Not divisible by 5");
+    }
+    
+    return 0;
+}
